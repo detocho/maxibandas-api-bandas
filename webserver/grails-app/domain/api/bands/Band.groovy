571418bd9f9b1f4523f70cc868cc242047326081
David Paz <dpaz@air-de-david.lan>
2014-08-24 01:03:40 -0500

    Se agregan las constraints de la clase de dominio

--- webserver/grails-app/domain/api/bands/Band.groovy
+++ webserver/grails-app/domain/api/bands/Band.groovy
@@ -3,31 +3,45 @@ package api.bands
 class Band {
 
     static constraints = {
+
+        categoryId      nullable:false, blank:false, maxSize: 20 , minSize: 1
+        name            nullable:false, blank:false, maxSize: 128, minSize: 10
+        priceMin        min:0F
+        priceMax        min:0F
+        currencyType    inList:['MXP','USD']
+        locationId      nullable: false, blank:false, maxSize: 20, minSize: 1
+        managerId       nullable:false, blank:false, maxSize: 20, minSize: 1
+        webPage         nullable: true, blank:true
+        description     nullable:false, blank:true, maxSize: 1024
+
+        typeItem        inList:['destacado','basico','free','promotor']
+        status          inList: ['active','closed','pending','paused']
+        dateDeleted     nullable: true
     }
 
-    String categoryId
-    String name
-    Float priceMin
-    Float priceMax
-    String currencyType
-    String locationId
-    List serviceLocations = []
-    List eventsTypes = []
-    String managerId
-    String webPage
-    List pictures = []
-    List urlVideos = []
-    String description
+    String  categoryId
+    String  name
+    Float   priceMin
+    Float   priceMax
+    String  currencyType
+    String  locationId
+    List    serviceLocations = []
+    List    eventsTypes = []
+    String  managerId
+    String  webPage
+    List    pictures = []
+    List    urlVideos = []
+    String  description
 
-    String typeItem
-    String status
+    String  typeItem
+    String  status
 
-    Date dateRegistration = new Date()
-    Date dateUpdate = new Date()
-    Date dateExpired = new Date() + 60
-    Date dateActivation = new Date()
-    Date dateRenovation = new Date()
-    Date dateDeleted 
+    Date    dateRegistration = new Date()
+    Date    dateUpdate = new Date()
+    Date    dateExpired = new Date() + 60
+    Date    dateActivation = new Date()
+    Date    dateRenovation = new Date()
+    Date    dateDeleted
 
 
 }
