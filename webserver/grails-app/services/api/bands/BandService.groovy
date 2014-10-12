f3bb54428f7d41e52136570deeecac1553d4414b
David Paz <dpaz@MacBook-Air-de-David.local>
2014-10-11 19:22:02 -0500

    Se agrega el servicio de search para listar las bandas por algunos filtros

--- webserver/grails-app/services/api/bands/BandService.groovy
+++ webserver/grails-app/services/api/bands/BandService.groovy
@@ -7,6 +7,10 @@ import bands.exceptions.NotFoundException
 import bands.exceptions.ConflictException
 import bands.exceptions.BadRequestException
 
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
 class BandService {
 
     static transactional = 'mongo'
@@ -127,7 +131,157 @@ class BandService {
 
     }
 
-    
+    def searchBand(params){
+
+        Map jsonResult = [:]
+        def result
+        def results = []
+
+        def SEARCH_PARAMS_MAP = [
+
+                manager_id:"managerId",
+                location_id:"locationId",
+                //events_type:"eventsTypes"
+                status: "status",
+                date_registration_from:"dateRegistration",
+                date_resgitration_to:"dateRegistration",
+                date_activation_from:"dateActivation",
+                date_activation_to:"dateActivation"
+                type_item:"typeItem"
+        ]
+
+        def date_registration_to
+        def date_registration_from
+        def date_activation_from
+        def date_activation_to
+
+        def queryMap = [:]
+
+
+        params.each{ key , value ->
+
+            def newKey = SEARCH_PARAMS_MAP[key]
+
+            if(newKey){
+                queryMap[newKey]=value
+            }
+        }
+
+        if (!queryMap){
+
+            jsonResult.total = 0
+            jsonResult.bands = []
+            return jsonResult
+        }
+
+        def bandCriteria = Band.createCriteria()
+
+        result = bandCriteria.list(){
+
+            params.each{ key, value ->
+
+                def newKey = SEARCH_PARAMS_MAP[key]
+
+                if (newKey && (newKey!='dateRegistration') && newKey!='dateActivation'){
+                    eq(newKey, value)
+                }
+            }
+
+            if (params.date_registration_from){
+
+                try {
+                    date_registration_from = ISODateTimeFormat.dateTimeParser().parseDateTime(params.date_registration_from).toDate()
+                    ge("dateRegistration", date_registration_from)
+                }catch(Exception e){
+                    throw new BadRequestException("Wrong date format in date_registration_from. Must be ISO json format")
+                }
+            }
+
+            if (params.date_registration_to){
+
+                try{
+                    date_registration_to = ISODateTimeFormat.dateTimeParser().parseDateTime(params.date_registration_to).toDate()
+                    le("dateRegistration",date_registration_to)
+                }catch(Exception e){
+                    throw new BadRequestException("Wrong date format in date_registration_to. Must be ISO json format")
+                }
+            }
+
+            if(params.date_registration_from && params.date_registration_to){
+
+                if (date_registration_to < date_registration_from){
+                    throw new BadRequestException("Invalid date range, date  date_registration_to must be greater than the  date date_registration_from")
+                }
+            }
+
+
+            if (params.date_activation_from){
+
+                try {
+                    date_activation_from = ISODateTimeFormat.dateTimeParser().parseDateTime(params.date_activation_from).toDate()
+                    ge("dateActivation", date_activation_from)
+                }catch(Exception e){
+                    throw new BadRequestException("Wrong date format in date_activation_from. Must be ISO json format")
+                }
+            }
+
+            if (params.date_ractivation_to){
+
+                try{
+                    date_activation_to = ISODateTimeFormat.dateTimeParser().parseDateTime(params.date_activation_to).toDate()
+                    le("dateActivation",date_activation_to)
+                }catch(Exception e){
+                    throw new BadRequestException("Wrong date format in date_activation_to. Must be ISO json format")
+                }
+            }
+
+            if(params.date_activation_from && params.date_activation_to){
+
+                if (date_activation_to < date_activation_from){
+                    throw new BadRequestException("Invalid date range, date  date_activation_to must be greater than the  date date_activation_from")
+                }
+            }
+        }
+
+        result.each{
+            results.add(
+                    
+                    id                   : it.id,
+                    category_id          : it.categoryId,
+                    name                 : it.name,
+                    title                : it.title,
+                    price_min            : it.priceMin,
+                    price_max            : it.priceMax,
+                    currency_type        : it.currencyType,
+                    location_id          : it.locationId,
+                    service_locations    : it.serviceLocations,
+                    events_types         : it.eventsTypes,
+                    manager_id           : it.managerId,
+                    web_page             : it.webPage,
+                    pictures             : it.pictures,
+                    url_videos           : it.urlVideos,
+                    description          : it.description,
+
+                    type_item            : it.typeItem,
+                    status               : it.status,
+
+                    date_registration    : it.dateRegistration,
+                    date_update          : it.dateUpdate,
+                    date_expired         : it.dateExpired,
+                    date_activation      : it.dateActivation,
+                    date_renovation      : it.dateRenovation,
+                    date_deleted         : (it.dateDeleted) ? it.dateDeleted : "null"   
+            )
+        }
+
+        jsonResult.total = results.size()
+        jsonResult.results = results
+
+        jsonResult
+
+    }
+
+
     
     def getResult(def band){
         
